4dc085f10d41335d566de5594bbb418626b025e5
owuordickson <owuordickson@gmail.com>
2020-10-26 10:56:09 +0100

    added

--- src/algorithms/dolpt/dlopt.py
+++ src/algorithms/dolpt/dlopt.py
@@ -11,9 +11,26 @@ Description: greedy heuristic algorithm that optimizes data lake jobs
 """
 
 import numpy as np
+import time
+from .daemon import Daemon
 
 
 class DlOpt:
 
     def __init__(self):
         self.jobs = np.array([])
+        self.demands = np.array([])
+        self.a_matrix = np.array([])
+        self.cost_matrix = np.array([])
+
+    def run(self):
+        while True:
+            print("running")
+            time.sleep(1)
+
+
+class DlService(Daemon):
+    def run(self):
+        # Or simply merge your code with MyDaemon.
+        your_code = DlOpt()
+        your_code.run()
