9396dd2974f635fe55f2242deaaf7cc97a18a751
owuorjnr <owuordickson@gmail.com>
2020-12-07 21:02:52 +0300

    added

--- src/algorithms/genetic/ga.py
+++ src/algorithms/genetic/ga.py
@@ -25,20 +25,52 @@ def run(problem, params):
     # Parameters
     maxit = params.maxit
     npop = params.npop
+    pc = params.pc
+    nc = np.round(pc * npop/2) * 2
+    gamma = params.gamma
+    mu = params.mu
+    sigma = params.sigma
 
     # Empty Individual Template
     empty_individual = structure()
     empty_individual.position = None
     empty_individual.cost = None
 
-    # BestSolution Ever Found
+    # Best Solution Ever Found
     bestsol = empty_individual.deepcopy()
+    bestsol.cost = np.inf
 
     # Initialize Population
     pop = empty_individual.repeat(npop)
-    for i in range(0, npop):
+    for i in range(npop):
         pop[i].position = np.random.uniform(varmin, varmax, nvar)
         pop[i].cost = costfunc(pop[i].position)
+        if pop[i].cost < bestsol.cost:
+            bestsol = pop[i].deepcopy()
+
+    # Best Cost of Iteration
+    bestcost = np.empty(maxit)
+
+    # Main Loop
+    for it in range(maxit):
+
+        popc = []
+        for k in range(nc//2):
+            # Select Parents
+            q = np.random.permutation(npop)
+            p1 = pop[q[0]]
+            p2 = pop[q[1]]
+
+            # Perform Crossover
+            c1, c2 = crossover(p1, p2, gamma)
+
+            # Perform Mutation
+            c1 = mutate(c1, mu, sigma)
+            c2 = mutate(c2, mu, sigma)
+
+            # Apply Bound
+            apply_bound(c1, varmin, varmax)
+            apply_bound(c2, varmin, varmax)
 
     # Output
     out = structure()
@@ -46,7 +78,24 @@ def run(problem, params):
     return out
 
 
+def crossover(p1, p2, gamma=0.1):
+    c1 = p1.deepcopy()
+    c2 = p1.deepcopy()
+    alpha = np.random.uniform(-gamma, 1+gamma, *c1.position.shape)
+    c1.position = alpha*p1.position + (1-alpha)*p2.position
+    c2.position = alpha*p2.position + (1-alpha)*p1.position
+    return c1, c2
 
 
+def mutate(x, mu, sigma):
+    y = x.deepcopy()
+    flag = np.random.rand(*x.position.shape) <= mu
+    ind = np.argwhere(flag)
+    y.position[ind] += sigma*np.random.rand(*ind.shape)
+    return y
 
+
+def apply_bound(x, varmin, varmax):
+    x.position = np.maximum(x.position, varmin)
+    x.position = np.minimum(x.position, varmax)
 
